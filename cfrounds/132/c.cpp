95df0d46ae7284e506b42c7f25022b503655aa0f
Pokegn <diegopipap@yahoo.com>
2025-09-01 23:33:29 -0600

    problem solved

--- cfrounds/132/c.cpp
+++ cfrounds/132/c.cpp
@@ -15,7 +15,45 @@ long long int pow2_lb(long long int x) { return (x == (x&-x) ? x : (2 << msb(x))
 
 void solve(){
     string s; cin >> s;
-    lli len = s.length();
+    lli n = s.length();
+    lli left = 0;
+    lli right = 0;
+    vlli qs;
+    forn(i, 0, n){
+        if(s[i] == '(') left++;
+        if(s[i] == ')') right++;
+        if(s[i] == '?') qs.push_back(i);
+    }
+    lli leftq = n/2 - left;
+    lli rightq = n/2 - right;
+    if(left+right == n || (right == n-right || left == n-left)){
+        cout << "YES" << nl;
+        return;
+    }
+    lli j = 0;
+    string r = "";
+    forn(i, 0, n){
+        if(s[i] != '?') r+=s[i];
+        else{
+            if(j<leftq-1) r+='(';
+            if(j == leftq-1) r+=')';
+            if(j == leftq) r+='(';
+            if(j > leftq) r+=')';
+            j++;
+        }
+    }
+
+    lli balance = 0;
+    forn(i, 0, n){
+        if(r[i] == '(') balance++;
+        else balance--;
+        if(balance < 0){
+            cout << "YES" << nl;
+            return;
+        }
+    }
+    cout << "NO" << nl;
+    return;
 }
 
 
