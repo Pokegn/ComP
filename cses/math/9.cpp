6d834716c0e930ece7735fb14cc24488f442dbe8
Pokegn <diegopipap@yahoo.com>
2026-02-19 19:03:46 -0600

    mobiusyei

--- cses/math/9.cpp
+++ cses/math/9.cpp
@@ -16,20 +16,60 @@ long long int pow2_lb(long long int x) { return (x == (x&-x) ? x : (2 << msb(x))
 
 int m = 1e6+1;
 vector<bool> is_prime(m+1, true);
-vector<int> primes;
+vector<ll> primes;
+vector<vector<ll>> d(1e6+1,vector<ll>(0));
 
 void solve(){
-    vector<ll> mobius(1e6+1);
-    mobius[1] = 1;
-    for(auto p:primes){
-        ll pot = p*p;
-        while(pot<1e6+1){
-
+    vector<ll> mobius(1e6+1, 1);
+    for(auto p: primes){
+        ll k = p;
+        while(k <= 1e6){
+            if(k%(p*p) == 0) mobius[k] = 0;
+            else mobius[k]*=-1;
+            k+=p;
         }
     }
+
+    vector<ll> count(1e6+1, 0);
+
+    ll n; cin >> n;
+    vector<ll> a(n); for(ll i=0; i<n; i++) cin >> a[i];
+
+    unordered_map<int, int> ina;
+    for(auto x: a) ina[x] = 1;
+
+    for (int i = 1; i * i < 1000001; i++) { //i y j/i dividen a j
+        for (int j = i * i; j < 1000001; j += i) {
+            if(!ina[j]) continue;
+            d[j].push_back(i);
+            if (j / i != i) d[j].push_back(j / i);  // Avoid duplicate for perfect squares
+        }
+    }
+
+    for(ll i=0; i<n; i++){
+        for(auto di: d[a[i]]){
+            count[di]++;
+        }
+    }
+
+    ll ans = 0;
+    for(ll i=1; i<=1e6; i++){
+        ans+= ((count[i]*(count[i]-1))/2)*mobius[i];
+    }
+
+    cout << ans << endl;
+    
+
+    //suma de i=0 a n-1 de suma de j>i de (mcd(a[i], a[j]) == 1
+    //= suma de i=0 a n-1 de suma de j>i de suma de d|a[i], a[j] de mobius(d) 
+    //= suma de d = 1 a 1e6 de mobius(d) * (multiplos de d en 2)
+
+
 }
 
 int main(){
+    cin.tie(0)->sync_with_stdio(false);
+    ll t=1;
     is_prime[0] = is_prime[1] = false;
     for (int i = 2; i <= m; i++) {
         if (is_prime[i] && (long long)i * i <= m) {
@@ -40,8 +80,6 @@ int main(){
     for(int i=2; i<=m; i++){
         if(is_prime[i]) primes.push_back(i);
     }
-    cin.tie(0)->sync_with_stdio(false);
-    ll t=1;
     //cin >> t;
     while(t--) solve();
     return 0;
