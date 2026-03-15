269d5f19f1c16dfebdce9c59a1062763456ec4f0
Pokegn <diegopipap@yahoo.com>
2026-03-15 10:13:39 -0600

    1800

--- cfrounds/743/c.cpp
+++ cfrounds/743/c.cpp
@@ -11,7 +11,44 @@ int msb(long long int x) { return 63 - __builtin_clzll(x);}
 long long int pow2_lb(long long int x) { return (x == (x&-x) ? x : (2 << msb(x)));}
 
 void solve(){
+    int n; cin >> n;
+    vector<vector<int>> a(n+1); //los que necesitan al weon i
+
     
+    vector<bool> vis(n+1, false);
+    vector<vector<int>> ans(n+1);
+    rep(i, 1, n+1){
+        int k; cin >> k;
+        if(k == 0) ans[0].push_back(i);
+        rep(j, 0, k){
+            int xd; cin >> xd;
+            a[xd].push_back(i);
+        } 
+    }
+
+    for(auto xd: a) sort(all(xd));
+    
+    vector<int> need(n+1, 0);
+    rep(i, 1, n+1){
+        need[i] = a[i].size();
+    }
+
+
+    rep(i, 0, n){
+        for(auto curr: ans[i]){//curr es un weon que puedo leer despues de i leidas
+            if(vis[curr]) continue;
+            vis[curr] = true;
+
+            for(auto j: a[curr]){//los que necesitan a curr
+                need[j]--;
+                if(need[j] == 0){
+
+                }
+            }
+        }
+    }
+
+
 }
 
 
