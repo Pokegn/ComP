7364c8d3e19c41da10c8bc327ed827f8001062e5
Pokegn <diegopipap@yahoo.com>
2026-05-21 08:32:05 -0600

    1099

---
+++ cfrounds/1099/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T> using minheap = priority_queue<T, vector<T>, greater<T>>;
+#define rep(i, a, b) for(int i=a; i<(b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define endl '\n'
+#define pb push_back
+#define fi first
+#define se second
+typedef long double ld;
+typedef long long ll;
+int msb(long long int x) { return 63 - __builtin_clzll(x);}
+long long int pow2_lb(long long int x) { return (x == (x&-x) ? x : (2 << msb(x)));}
+
+void solve(){
+    
+}
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(false);
+    int t=1; 
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
