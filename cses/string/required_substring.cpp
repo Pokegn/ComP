de72d1135469feee6fe37645475fe5c63f7deea2
Pokegn <diegopipap@yahoo.com>
2026-09-26 11:24:51 -0600

    cses
    cses

--- cses/string/required_substring.cpp
+++ cses/string/required_substring.cpp
@@ -35,30 +35,67 @@ void solve(){
 
     vector<ll> rots; //todos los que tengo que checar atras ademas de mi coso
 
-    string r;
-    rep(i, 0, m){
-        r = s;
-        rotate(r.begin(), r.end() - i, r.end()); //rotar uno a la derecha
-        if(r == s) rots.push_back(i);
+    rep(i, 1, m){
+        string firsti = "", lasti = "";
+        
+        rep(j, 0, i){
+            firsti+=s[j];
+            lasti+=s[m-i+j];
+        }
+        
+        if(firsti == lasti) rots.push_back(i);
     }
+    
+    string r;
+    // rep(i, 1, m){
+    //     r = s;
+    //     rotate(r.begin(), r.end() - i, r.end()); //rotar uno a la derecha
+    //     // if(r == s) cout << "AA " << i << endl;
+    // }
+
+    // for(auto xd: rots) cout << xd << ' ';
+    // cout << endl;
 
     vector<ll> countfinal(n, 0); //en cuantos la primera vez que hay un s es acabando hasta i
     vector<ll> countnunca(n, 0); //en cuantos no hay ningun s de 0 a i
     ll tot = 0;
 
+    //para que este por primera vez acabando en i, entonces
+    //los primeros i-m no tienen nada
+    //y luego le quito los que tienen en alguno de rot
+    //pero tiene que ser en alguno
+
     rep(i, 0, n){
         tot = fexp(26, n, MOD);
-        for(auto j: rots)
-        if(i>=m-1){
-            
-            countfinal[i] = fexp(26, i+1-m, MOD)-countfinal[i+1-m];
-            countfinal[i] = (countfinal[i]%MOD+MOD)%MOD;
+        if(i-m < -1){
+            countfinal[i] = 0;
+        }
+        else{
+            if(i-m == -1) countfinal[i] = 1;
+            else{
+                countfinal[i] = countnunca[i-m];
+            }
+        }
 
-        } 
+        for(auto j: rots){ //en lugar de checar el i-m checo el i-m+j
+            if(i-2*m+j == -1) countfinal[i] --;
+            if(i-2*m+j >= 0) countfinal[i] -= countfinal[i-m+j];
+            countfinal[i]%=MOD;
+            countfinal[i] = (countfinal[i] + MOD)%MOD;
+        }
 
         if(i == 0) countnunca[i] = 26 - countfinal[0];
         else countnunca[i] = ((countnunca[i-1]*26 - countfinal[i])%MOD + MOD)%MOD;
     }
+
+    ll ans = 0;
+    rep(i, 0, n){
+        countfinal[i] = (countfinal[i]*fexp(26, n-i-1, MOD))%MOD;
+        // cout << countfinal[i] << ' ' << countnunca[i] << endl;
+        ans += countfinal[i]; ans%=MOD;
+    }
+    cout << ans << endl;
+    return;
 }
 
 
